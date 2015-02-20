9560ce7d22457d5a7f6cc07bef0ce2b9b74955a5
lyashenkoav92@gmail.com <lyashenkoav92@gmail.com@a48bbefb-d17f-25e1-05e3-0a94f7aebdbd>
2015-02-20 20:33:21 +0000

    Configuration module uploading

---
+++ Configuration/trunk/src/main/java/com/anli/configuration/ConfigSnapshot.java
@@ -0,0 +1,21 @@
+package com.anli.configuration;
+
+import java.util.Map;
+
+public class ConfigSnapshot {
+    
+    protected String group;
+    protected Map<String, String> properties;
+    
+    public ConfigSnapshot(String group, Map<String, String> properties) {
+        this.properties = properties;
+    }
+    
+    public String getProperty(String id) {
+        return properties.get(id);
+    }
+    
+    public String getGroup() {
+        return group;
+    }
+}
