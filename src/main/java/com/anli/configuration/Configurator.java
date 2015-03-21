83d259a492e3fb17d0ced8ea4224a2498ea8aa96
lyashenkoav92@gmail.com <lyashenkoav92@gmail.com@a48bbefb-d17f-25e1-05e3-0a94f7aebdbd>
2015-03-21 17:08:23 +0000

    Global formatting (no actual logic changes)

--- src/main/java/com/anli/configuration/Configurator.java
+++ src/main/java/com/anli/configuration/Configurator.java
@@ -1,14 +1,20 @@
 package com.anli.configuration;
 
 import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Configurator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Configurator.class);
+
     // DEBUG STUB
     public static ConfigSnapshot getConfig(String group) {
         if ("db".equals(group)) {
             return new ConfigSnapshot(group, Collections.singletonMap("connection_pool", "jdbc/BSA"));
         } else {
+            LOG.error("Could not resolve configuration group {}", group);
             throw new RuntimeException();
-        }      
+        }
     }
 }
